c30a6eefad091ddc67247d4445af5eea3913abbb
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-06-29 23:33:53 +0200

    worked on landing page and css, almost finished

--- assets/Main.js
+++ assets/Main.js
@@ -1,25 +1,27 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import HelloWorld from "./pages/HelloWorld"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Register from "./pages/Register";
 
 function Main() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<HelloWorld />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default Main
+export default Main;
 
 if (document.getElementById("app")) {
-  const root = ReactDOM.createRoot(document.getElementById("app"))
+  const root = ReactDOM.createRoot(document.getElementById("app"));
   root.render(
     <React.StrictMode>
       <Main />
     </React.StrictMode>
-  )
+  );
 }
