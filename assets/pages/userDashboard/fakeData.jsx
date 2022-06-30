5ea581a84318bcc201c58caf739039ba5be0b1cc
Oscar AGUDELO <oscar.agudelo.pro@gmail.com>
2022-06-30 11:17:20 +0200

    nav bar ok

--- assets/pages/userDashboard/fakeData.jsx
+++ assets/pages/userDashboard/fakeData.jsx
@@ -1,9 +1,4 @@
-import React, { useState } from "react"
-import "./userDashboard.css";
-import Nav from "../../components/nav/Nav";
-
-function UserDashboard() {
-  const [projets, setProjets] = useState([{
+const projets = [{
     id:1,
     name: "Les avocats chinois . ,",
     description: "Assotiation des avocats, en lutte pour une meilleur porduction des avocats d'origine chinois",
@@ -33,7 +28,7 @@ function UserDashboard() {
     categoriy_id: 1,
     agency_id: 1, 
   },
-{
+  {
     id:1,
     name: "Les avocats chinois . ,",
     description: "Assotiation des avocats, en lutte pour une meilleur porduction des avocats d'origine chinois",
@@ -42,17 +37,4 @@ function UserDashboard() {
     status: "todo",
     categoriy_id: 1,
     agency_id: 1, 
-  }]);
-  return (
-    <div id="userDashboardContainer">
-      <Nav />
-      <div className="allProjetsContainer">
-        {projets.map((projet) => 
-        <p>{projet.name}</p>
-        )}
-      </div>
-    </div>
-  );
-}
-
-export default UserDashboard
+  }];
