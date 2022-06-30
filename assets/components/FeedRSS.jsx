c9f32e7066a9cf2168db5769f95133c4346f4e6b
Alexandre Pillias <alexandre.pillias@gmail.com>
2022-06-30 16:40:41 +0200

    functional filters

--- assets/components/FeedRSS.jsx
+++ assets/components/FeedRSS.jsx
@@ -11,6 +11,8 @@ function FeedRSS() {
       author: "Loris Chastanet",
       category: "Finance",
       title: "Every",
+      date: "06/04/2019",
+      agency: "Paris",
       desc: "Every.io lets you set up and manage all your Finance and HR operations in one place — Bank Accounts, Corporate Cards, Bill Pay, HR, Payroll, Benefits, and Web3 Wallets. We’re super friendly to Web3 and international founders. Get started in under 10 minutes.",
     },
     {
@@ -19,6 +21,8 @@ function FeedRSS() {
       author: "Anthony Gorsky",
       category: "IT",
       title: "Shift",
+      date: "28/09/2020",
+      agency: "Marseilles",
       desc: "Convert text or SVG file to the 3D, right in your browser. Build cool 3D animations for your website, app, social media, or thumbnails. It's super easy: Upload → Customize → Animate.",
     },
     {
@@ -27,6 +31,8 @@ function FeedRSS() {
       author: "Julien Richard",
       category: "Music",
       title: "WiredVibe",
+      date: "14/12/2017",
+      agency: "Nantes",
       desc: "WiredVibe is a neuroscience-based music platform that improves your focus in less than 10 minutes. Our technology generates personalized sound therapies using health data (via Google), and weather conditions based on your current location.",
     },
     {
@@ -35,6 +41,8 @@ function FeedRSS() {
       author: "Jean-François Morin",
       category: "IT",
       title: "Coldbrew",
+      date: "31/01/2022",
+      agency: "Lilles",
       desc: "A fun little app I built over a weekend. Select your favorite macOS apps using a visual interface. Then install them with a single terminal command (apps will install with Homebrew). Share a link to your selection with others, or bookmark it for later.",
     },
   ]);
@@ -48,12 +56,16 @@ function FeedRSS() {
               <img src={Person} alt={data.author} className="feed-img" />
               <h2 className="feed-title">{data.author}</h2>
               <div className="flex-category">
-                <h2 className="feed-title">{data.category}</h2>
+                <h2 className="feed-title">
+                  From {data.agency}, in {data.category}
+                </h2>
               </div>
             </div>
             <div className="flex-desc">
               <div className="feed-desc">
-                <h2 className="desc-title">{data.title}</h2>
+                <h2 className="desc-title">
+                  {data.title}, launched {data.date}
+                </h2>
                 <p className="desc-text">{data.desc}</p>
               </div>
             </div>
--- assets/pages/FilterProjects.jsx
+++ assets/pages/FilterProjects.jsx
@@ -14,6 +14,8 @@ function FilterProjects() {
       author: "Loris Chastanet",
       category: "Finance",
       title: "Every",
+      date: "06/04/2019",
+      agency: "Paris",
       desc: "Every.io lets you set up and manage all your Finance and HR operations in one place — Bank Accounts, Corporate Cards, Bill Pay, HR, Payroll, Benefits, and Web3 Wallets. We’re super friendly to Web3 and international founders. Get started in under 10 minutes.",
     },
     {
@@ -22,6 +24,8 @@ function FilterProjects() {
       author: "Anthony Gorsky",
       category: "IT",
       title: "Shift",
+      date: "28/09/2020",
+      agency: "Marseilles",
       desc: "Convert text or SVG file to the 3D, right in your browser. Build cool 3D animations for your website, app, social media, or thumbnails. It's super easy: Upload → Customize → Animate.",
     },
     {
@@ -30,6 +34,8 @@ function FilterProjects() {
       author: "Julien Richard",
       category: "Music",
       title: "WiredVibe",
+      date: "14/12/2017",
+      agency: "Nantes",
       desc: "WiredVibe is a neuroscience-based music platform that improves your focus in less than 10 minutes. Our technology generates personalized sound therapies using health data (via Google), and weather conditions based on your current location.",
     },
     {
@@ -38,9 +44,28 @@ function FilterProjects() {
       author: "Jean-François Morin",
       category: "IT",
       title: "Coldbrew",
+      date: "31/01/2022",
+      agency: "Lilles",
       desc: "A fun little app I built over a weekend. Select your favorite macOS apps using a visual interface. Then install them with a single terminal command (apps will install with Homebrew). Share a link to your selection with others, or bookmark it for later.",
     },
   ]);
+  const [agencies, setAgencies] = useState("");
+  const [themes, setThemes] = useState("");
+  const [dates, setDates] = useState("");
+  const [words, setWords] = useState("");
+
+  const handleAgencies = (e) => {
+    setAgencies(e.target.value);
+  };
+  const handleThemes = (e) => {
+    setThemes(e.target.value);
+  };
+  const handleDates = (e) => {
+    setDates(e.target.value);
+  };
+  const handleWords = (e) => {
+    setWords(e.target.value);
+  };
 
   return (
     <div className="filter-page">
@@ -48,32 +73,88 @@ function FilterProjects() {
       <div className="filter-desc">
         {/* <div className="filter-flex"> */}
         <div className="filter-top-flex">
-          <div className="flex-top-container"></div>
-          <div className="flex-top-container"></div>
-          <div className="flex-top-container"></div>
-          <div className="flex-top-container"></div>
+          <div className="flex-top-container">
+            <h2 className="filter-title">Key-words</h2>
+            <input
+              placeholder="Search for key-words"
+              className="filter-input"
+              onChange={handleWords}
+            />
+          </div>
+          <div className="flex-top-container">
+            <h2 className="filter-title">Theme</h2>
+            <select className="filter-select" onChange={handleThemes}>
+              <option value="" selected>
+                Choose a theme
+              </option>
+              <option>Medical</option>
+              <option>IT</option>
+              <option>Finance</option>
+              <option>Music</option>
+            </select>
+          </div>
+          <div className="flex-top-container">
+            <h2 className="filter-title">Date</h2>
+            <input
+              placeholder="Select a date"
+              className="filter-input"
+              onChange={handleDates}
+            />
+          </div>
+          <div className="flex-top-container">
+            <h2 className="filter-title">Status</h2>
+          </div>
         </div>
         <div className="filter-bottom-flex">
-          <div className="flex-bottom-container"></div>
+          <div className="flex-bottom-container">
+            <h2 className="filter-title">Agency</h2>
+            <select className="filter-select" onChange={handleAgencies}>
+              <option value="" selected>
+                Choose an agency
+              </option>
+              <option>Lilles</option>
+              <option>Paris</option>
+              <option>Marseilles</option>
+              <option>Nantes</option>
+            </select>
+          </div>
           <div className="flex-component-container">
             <div className="feed-outer-flex">
-              {fake.map((data) => (
-                <div className="feed-container" key={data.id}>
-                  <div className="feed-flex">
-                    <img src={Person} alt={data.author} className="feed-img" />
-                    <h2 className="feed-title">{data.author}</h2>
-                    <div className="flex-category">
-                      <h2 className="feed-title">{data.category}</h2>
+              {fake
+                .filter(
+                  (elem) =>
+                    (elem.agency.includes(agencies) &&
+                      elem.category.includes(themes) &&
+                      elem.date.includes(dates) &&
+                      elem.title.includes(words)) ||
+                    elem.desc.includes(words) ||
+                    elem.author.includes(words)
+                )
+                .map((data) => (
+                  <div className="feed-container" key={data.id}>
+                    <div className="feed-flex">
+                      <img
+                        src={Person}
+                        alt={data.author}
+                        className="feed-img"
+                      />
+                      <h2 className="feed-title">{data.author}</h2>
+                      <div className="flex-category">
+                        <h2 className="feed-title">
+                          From {data.agency}, in {data.category}
+                        </h2>
+                      </div>
+                    </div>
+                    <div className="flex-desc">
+                      <div className="feed-desc">
+                        <h2 className="desc-title">
+                          {data.title}, launched {data.date}
+                        </h2>
+                        <p className="desc-text">{data.desc}</p>
+                      </div>
                     </div>
                   </div>
-                  <div className="flex-desc">
-                    <div className="feed-desc">
-                      <h2 className="desc-title">{data.title}</h2>
-                      <p className="desc-text">{data.desc}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
